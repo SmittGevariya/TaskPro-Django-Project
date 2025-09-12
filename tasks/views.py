ead60ba9426e50df4ad9a2a80d771c60d1f4593a
SmittGevariya <smitgevariya.sg141@gmail.com>
2025-09-12 15:25:48 +0530

    feat:create TaskViewSet for core API logic and ownership

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 # Create your views here.
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class=TaskSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        user=self.request.user
+        return user.tasks.all().order_by('due_date')
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
