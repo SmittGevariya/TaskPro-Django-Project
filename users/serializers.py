3470087bb254dda9f60ecf49bc3e6fc70b81cbfe
SmittGevariya <smitgevariya.sg141@gmail.com>
2025-09-12 16:51:19 +0530

    fix : resolve circular import in URL configuration

--- users/serializers.py
+++ users/serializers.py
@@ -13,8 +13,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         user=User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            firstname=validated_data.get('firstname',''),
-            lastname=validated_data.get('lastname','')
+            first_name=validated_data.get('first_name',''),
+            last_name=validated_data.get('last_name','')
         )
         user.set_password(validated_data['password'])
         user.save()
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from taskpro_project.urls import urlpatterns
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
