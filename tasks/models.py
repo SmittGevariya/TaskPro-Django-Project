1ca3048bc9bd662e6f64cd841e618046d8d13c0c
SmittGevariya <smitgevariya.sg141@gmail.com>
2025-09-12 18:06:42 +0530

    fixed is_completed field in Task model

--- tasks/models.py
+++ tasks/models.py
@@ -13,7 +13,7 @@ class Task(models.Model):
     description=models.TextField(blank=True,null=True)
     due_date=models.DateField()
     priority=models.CharField(max_length=10,choices=PRIORITY_CHOICES,default='Medium')
-    id_completed=models.BooleanField(default=False)
+    is_completed=models.BooleanField(default=False)
     owner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='tasks')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
