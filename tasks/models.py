271e3fadaed7e2582cf6f0c4f3aa5cb3fb59ee25
SmittGevariya <smitgevariya.sg141@gmail.com>
2025-09-12 15:09:06 +0530

    feat:create task model and run initial migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
+from random import choices
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES=[
+        ('Low','Low'),
+        ('Medium','Medium'),
+        ('High','High'),
+    ]
+
+    title=models.CharField(max_length=200)
+    description=models.TextField(blank=True,null=True)
+    due_date=models.DateField()
+    priority=models.CharField(max_length=10,choices=PRIORITY_CHOICES,default='Medium')
+    id_completed=models.BooleanField(default=False)
+    owner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='tasks')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
